f6c947742e71382c5bec9c384a2b39091ab56c87
Jonathan Bell <jon@jonbell.net>
2021-12-22 14:34:56 +0000

    checks

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,32 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import { wait } from './wait'
 
 async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    // core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const octokit = github.getOctokit(core.getInput("GITHUB_TOKEN"));
+    const repo = {
+      ...github.context.repo,
+    }
+    if (core.getInput("repo")) {
+      const manualRepo = core.getInput("repo").split("/");
+      repo.owner = manualRepo[0];
+      repo.repo = manualRepo[1];
+    }
+    let head_sha = github.context.sha;
+    if (core.getInput("head_sha")) {
+      head_sha = core.getInput("head_sha");
+    }
+    const req = {
+      ...repo,
+      name: "Testing checks API",
+      head_sha,
+    }
+    const resp = await octokit.rest.checks.create(req);
+    console.log(JSON.stringify(resp, null, 2));
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
